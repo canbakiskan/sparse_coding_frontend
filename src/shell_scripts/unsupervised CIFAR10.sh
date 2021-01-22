0d167a98c2d163a744e5d61005dfaa601d990df0
Can Bakiskan <canbakiskan@ucsb.edu>
2021-01-21 20:05:23 -0800

    removed .py

--- src/shell_scripts/unsupervised CIFAR10.sh
+++ src/shell_scripts/unsupervised CIFAR10.sh
@@ -1,21 +1,21 @@
 #!/bin/bash 
 
-COMMAND="python -m neuro-inspired-defense.src.train_autoencoder.py"
+COMMAND="python -m neuro-inspired-defense.src.train_autoencoder"
 echo $COMMAND
 eval $COMMAND
 
-COMMAND="python -m neuro-inspired-defense.src.train_classifier.py"
+COMMAND="python -m neuro-inspired-defense.src.train_classifier"
 echo $COMMAND
 eval $COMMAND
 
-COMMAND="python -m neuro-inspired-defense.src.run_attack.py"
+COMMAND="python -m neuro-inspired-defense.src.run_attack"
 echo $COMMAND
 eval $COMMAND
 
-COMMAND="python -m neuro-inspired-defense.src.run_attack.py --attack_whitebox_type=W-NFGA"
+COMMAND="python -m neuro-inspired-defense.src.run_attack --attack_whitebox_type=W-NFGA"
 echo $COMMAND
 eval $COMMAND
 
-COMMAND="python -m neuro-inspired-defense.src.run_attack.py --attack_whitebox_type=W-NFGA --attack_num_steps=100 --attack_step_size=0.00196 "
+COMMAND="python -m neuro-inspired-defense.src.run_attack --attack_whitebox_type=W-NFGA --attack_num_steps=100 --attack_step_size=0.00196 "
 echo $COMMAND
 eval $COMMAND
