121b551583ba32199fd9491e94a90ea067fe6746
Can Bakiskan <canbakiskan@ucsb.edu>
2021-04-04 00:49:33 -0700

    removed shell scripts

--- src/shell_scripts/not sure if working/new_4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pid=320605 
-while [ -d /proc/$pid ] ; do
-    sleep 1
-done 
-
-export CUDA_VISIBLE_DEVICES=0
-python run_attack.py --attack_skip_clean --attack_whitebox_type=W-NFGA --attack_quantization_BPDA_steepness=4
