f0b5960c94d2fc2a72e7d7539c660b90b7d2363c
Can Bakiskan <canbakiskan@ucsb.edu>
2021-05-07 20:02:24 -0700

    fixed typos

--- src/shell_scripts/by_defense/PGD_AT.sh
+++ src/shell_scripts/by_defense/PGD_AT.sh
@@ -4,7 +4,7 @@ full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 
 source "$dir_path/base_commands.sh"
-defense_args="--neural_net_epochs=100 --neural_net_no_frontend --adv_training_method=PGD --neural_net_epochs=100 --neural_net_lr_max=0.05"
+defense_args="--neural_net_epochs=100 --neural_net_no_frontend --adv_training_method=PGD --neural_net_lr_max=0.05"
 
 for base_command in "${base_commands[@]}"
 do
