4598c36d4c5926165a1b994fce1be04c72af28c8
Can Bakiskan <canbakiskan@ucsb.edu>
2021-05-07 20:26:35 -0700

    removed neural_net_epochs=100, fixed typos

--- src/shell_scripts/by_attack/defense_args.sh
+++ src/shell_scripts/by_attack/defense_args.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 our_defense=""
-natural="--neural_net_epochs=100 --neural_net_no_frontend --neural_net_lr_max=0.05"
-PGD_AT="--neural_net_epochs=100 --neural_net_no_frontend --adv_training_method=PGD --neural_net_epochs=100 --neural_net_lr_max=0.05"
-TRADES="--neural_net_epochs=100 --neural_net_no_frontend --adv_training_method=TRADES --neural_net_epochs=76 --neural_net_lr0.01 --lr_scheduler=step"
+natural="--neural_net_no_frontend --neural_net_lr_max=0.05"
+PGD_AT="--neural_net_no_frontend --adv_training_method=PGD --neural_net_lr_max=0.05"
+TRADES="--neural_net_no_frontend --adv_training_method=TRADES --neural_net_lr=0.01 --neural_net_lr_scheduler=step"
 
 defense_args=("$our_defense" "$natural" "$PGD_AT" "$TRADES")
--- src/shell_scripts/by_defense/TRADES.sh
+++ src/shell_scripts/by_defense/TRADES.sh
@@ -4,7 +4,7 @@ full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 
 source "$dir_path/base_commands.sh"
-defense_args="--neural_net_no_frontend --adv_training_method=TRADES --neural_net_epochs=76 --neural_net_lr=0.01 --lr_scheduler=step"
+defense_args="--neural_net_no_frontend --adv_training_method=TRADES --neural_net_lr=0.01 --neural_net_lr_scheduler=step"
 
 for base_command in "${base_commands[@]}"
 do
